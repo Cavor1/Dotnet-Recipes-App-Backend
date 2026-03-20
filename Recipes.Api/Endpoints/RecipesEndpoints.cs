b2dcab6ef6abd2c167de225c1d8d7adf2e084b03
Cavor1 <cavor1111@gmail.com>
2026-03-20 12:30:44 +0100

    update recipe endpoint

--- Recipes.Api/Endpoints/RecipesEndpoints.cs
+++ Recipes.Api/Endpoints/RecipesEndpoints.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Reflection.Metadata;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using Recipes.Api.Data;
 using Recipes.Api.Dto;
@@ -58,7 +59,8 @@ public static class RecipesEndpoints
     //[TODO]might have concurrency problems, between getting existing ingredients and saving new ingredients some time passes.
     static async Task<IResult> CreateRecipe(CreateRecipeDto req, AppDbContext db)
     {
-    
+        
+        //validation
         var reqValidation = req.Validate();
         if (reqValidation is not null) return reqValidation;
 
@@ -70,9 +72,25 @@ public static class RecipesEndpoints
 
         var reqIngredients = req.RecipeIngredients.Select(r => new
         {
-            Name = r.Name.Trim().ToLower(),
+            Name = r.Name.Trim().ToLowerInvariant(),
             Quantity = r.Quantity
         }).ToList();
+
+        //check for duplicates, is there are, bad request
+        var duplicateNames = reqIngredients
+            .GroupBy(x => x.Name)
+            .Where(g => g.Count() > 1)
+            .Select(g => g.Key)
+            .ToList();
+
+        if (duplicateNames.Count > 0)
+        {
+            return Results.BadRequest(new
+            {
+                Error = "Duplicate ingredient names in request",
+                Ingredients = duplicateNames
+            });
+        }
 
         //get existing ingredients
         //create and add nonexistent
@@ -124,8 +142,89 @@ public static class RecipesEndpoints
         return Results.Created($"/recipes/{recipe.Id}", new {Id = recipe.Id});//[TODO] can retturn better response
     }
 
-    static async Task<IResult> UpdateRecipe(CreateRecipeDto req,Guid id,AppDbContext db)
+    static async Task<IResult> UpdateRecipe(Guid id, CreateRecipeDto req,AppDbContext db)
     {
-        return Results.StatusCode(501);
+        //validation
+        var reqValidation = req.Validate();
+        if (reqValidation is not null) return reqValidation;
+
+        foreach (var ri in req.RecipeIngredients)
+        {
+            var riValidation = ri.Validate();
+            if (riValidation is not null) return riValidation;
+        }
+
+        //getting recipe and return if not found by id
+        var recipe = await db.Recipes.Where(r => r.Id == id).FirstOrDefaultAsync();
+
+        if (recipe is null) return Results.NotFound();
+
+        var reqIngredients = req.RecipeIngredients.Select(r => new
+        {
+            Name = r.Name.Trim().ToLowerInvariant(),
+            Quantity = r.Quantity
+        }).ToList();
+
+        //check for duplicates, is there are, bad request
+        var duplicateNames = reqIngredients
+            .GroupBy(x => x.Name)
+            .Where(g => g.Count() > 1)
+            .Select(g => g.Key)
+            .ToList();
+
+        if (duplicateNames.Count > 0)
+        {
+            return Results.BadRequest(new
+            {
+                Error = "Duplicate ingredient names in request",
+                Ingredients = duplicateNames
+            });
+        }
+
+        //get existing ingredients
+        var existingIngredients = await db.Ingredients
+            .Where(ei => reqIngredients
+                .Select(ri => ri.Name)
+                .Contains(ei.Name)) //list of reqIngredients.Name contains ei.Name
+            .ToDictionaryAsync(i => i.Name);      
+
+        //set new name and description of recipe
+        recipe.Title = req.Title;
+        recipe.Description = req.Description;
+
+
+        await db.RecipeIngredients.Where(ri => ri.RecipeId == id).ExecuteDeleteAsync();
+
+        //add recipe ingredients and new ingredients
+        foreach (var reqIngredient in reqIngredients)
+        {
+
+            var recipeIngredient = new RecipeIngredient()
+            {
+                RecipeId = recipe.Id,
+                Quantity = reqIngredient.Quantity
+            };
+
+            if (!existingIngredients.ContainsKey(reqIngredient.Name))
+            {
+                var newIngredient = new Ingredient()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = reqIngredient.Name
+                };
+                db.Ingredients.Add(newIngredient);
+                recipeIngredient.IngredientId = newIngredient.Id;
+            }
+            else
+            {
+                recipeIngredient.IngredientId = existingIngredients[reqIngredient.Name].Id;
+            }
+            db.RecipeIngredients.Add(recipeIngredient);
+
+        }
+        await db.SaveChangesAsync();
+
+
+        return Results.Ok();
     }
 }
